afcab94e89f780a849c8e225ccc7f68effae8494
lenovo <lenovo@192.168.1.64>
2025-07-22 15:42:27 +0530

    Added m1() method in Demo.java by Akshay

--- MethodPractice/src/com/task1/Demo.java
+++ MethodPractice/src/com/task1/Demo.java
@@ -1,5 +1,12 @@
+package com.task1;
+
 public class Demo
-  {
+  { 
+	public void m1()
+	{
+		System.out.println("m1() of demo ...!!");
+	}
+	
     public static void main(String args[])
     {
       System.out.println("This is Demo file");
